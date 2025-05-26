9f52191aa05bc4ee5118f36017b493fb5105d05a
Simer <simerseehra13@gmail.com>
2025-05-26 17:43:33 +0530

    adsense2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,11 +26,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT_ID}`}
+          crossOrigin="anonymous"
+          strategy="beforeInteractive"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=$(process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT_ID)"
-     crossOrigin="anonymous"></script>
+        
         {children}
       </body>
     </html>
